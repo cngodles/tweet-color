b29623d33fd6b3dbe0fc788ed031afb5aeec7a17
Clinton N,. Godlesky <cngodles@gmail.com>
2016-04-29 00:15:45 -0400

    blank template

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>Page Title</title>
+  <link rel="shortcut icon" href="favicon.ico">
+  <link rel="icon" href="favicon.ico">
+  <link rel="stylesheet" type="text/css" href="styles.css">
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+<body>
+<header>
+	<div>
+    	Header Content
+    </div>
+	<nav>
+    	<a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+        <a href="#">Link 4</a>
+    </nav>
+</header>
+<main>Main Content</main>
+<footer>Footer</footer>
+</body>
+</html>
